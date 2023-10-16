30c3721825ac19dfceb868153c905394fed16a22
Khadaassi <aassikhadija@yahoo.fr>
2023-10-16 02:02:12 -0700

    task 6 low level prog

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,10 +3,9 @@
 int main(void)
 {
 	printf("Size of char: %lu byte(s)\n", sizeof(char));
-	printf("Size of short: %lu byte(s)\n", sizeof(short));
-	printf("Size of int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long: %lu byte(s)\n", sizeof(long));
-	printf("Size of long long: %lu byte(s)\n", sizeof(long long));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of long long int: %lu byte(s)\n", sizeof(long long));
 	printf("Size of float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
