a906dfc4b2d2036fffdab2edd114bc589444a5f5
Khadaassi <aassikhadija@yahoo.fr>
2023-10-16 02:11:10 -0700

    task 4 low level prog

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 
-	/**
-	 * main - Displays the string using the puts fonction
-	 *
-	 * Description: Uses the main function to display
-	 * "Programming is like building a puzzle".
-	 *
-	 * Return: Always returns 0 (success)
-	 *
-	 **/
+/**
+* main - Displays the string using the puts function
+*
+* Description: Uses the main function to display
+* "Programming is like building a puzzle".
+*
+* Return: Always returns 0 (success)
+*
+**/
 int main(void)
 	{
 	    puts("\"Programming is like building a multilingual puzzle");
