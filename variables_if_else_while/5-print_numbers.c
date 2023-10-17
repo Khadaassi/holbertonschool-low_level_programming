6e4676930d4b808f5ac36316b6737b18b475f897
Khadaassi <aassikhadija@yahoo.fr>
2023-10-17 11:43:09 -0700

    task 5 variable

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single digit numbers of base 10 
+ *
+ * Description: prints all single digit numbers of base 10 
+ * starting from 0, followed by a new line
+ *
+ * Return:0
+ */
+
+int main (void)
+
+{
+	int number;
+
+	for (number = 0; number <= 9; number++) 
+	{
+		printf("%d", number);
+	}
+	printf("\n");
+    return (0);
+}
