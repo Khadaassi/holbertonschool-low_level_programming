20da27d8f1abd6750126071da0a817640b313684
Khadaassi <aassikhadija@yahoo.fr>
2023-10-17 10:36:49 -0700

    task 2 variable

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -3,20 +3,21 @@
 /**
  * main - prints the alphabet in lowercase
  *
- * Description: using the main function, this program prints the alphabet in lowercase
+ * Description: using the main function, 
+ * this program prints the alphabet in lowercase
  *
  * Return: 0
  */
 int main(void)
 {
-    char ch;
+	char ch;
 
-    for (ch = 'a'; ch <= 'z'; ch++)
-    {
-        putchar(ch);
-    }
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+	putchar(ch);
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
