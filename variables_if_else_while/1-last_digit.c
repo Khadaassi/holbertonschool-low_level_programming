2c2a510524e210c768b63a68ca1bd9b82ba2b4f8
Khadaassi <aassikhadija@yahoo.fr>
2023-10-17 05:28:39 -0700

    task 1 variable

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,12 +2,12 @@
 #include <time.h>
 #include <stdio.h>
 
-/*
+/**
  * main - prints the last digit of ther number stored in the variable
- * 
- * Description:using the main function 
- * this program prints the last digit and a comment
- **/
+ *
+ *Description:using the main function 
+ *this program prints the last digit and a comment
+ */
 
 int main(void)
 {
@@ -20,15 +20,15 @@ int main(void)
 
 	if (mod > 5)
 	{
-		printf("Last digit of %d is %d and is greater than 5\n",n,mod);
+		printf("Last digit of %d is %d and is greater than 5\n", n, mod);
 	}
 	if (mod == 0)
 	{
-		printf("Last digit of %d is %d and is 0\n",n,mod);
+		printf("Last digit of %d is %d and is 0\n", n, mod);
 	}
 	if (mod < 6 && mod != 0)
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n",n,mod);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, mod);
 	}
 	return (0);
 }
