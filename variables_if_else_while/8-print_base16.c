c9cd7b2f077538305e92b239c37e3d68fa1ed680
Khadaassi <aassikhadija@yahoo.fr>
2023-10-18 00:18:00 -0700

    task 8 variable

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
+/** main - prints all the numbers of base 16 in lowercase
+ *
+ * Description: using fonction main print all the numbers
+ * of base 16 in loweercase followed by a new line
+ *
+ * Return: 0
+ */
+
 int main(void)
+
 {
 	int i;
+
 	for (i = 0; i < 16; i++)
 	{
 		char hexDigit;
+
 		if (i < 10)
 		{
-		hexDigit = i +'0';
+		hexDigit = i + '0';
 		}
 		else
 		{
