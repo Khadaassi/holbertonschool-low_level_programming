71e321e90fb10a56a6d70db982dcd86884254291
Khadaassi <aassikhadija@yahoo.fr>
2023-10-18 01:20:37 -0700

    task 0 function

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints _putchar followed by a new line
+ *
+ * Description: using the main function, print _putchar, 
+ * followed by a new line.
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+	return (0);
+}
+
