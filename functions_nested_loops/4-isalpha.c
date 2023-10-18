61e36a43de33d040e626551a8301099874cac1ed
Khadaassi <aassikhadija@yahoo.fr>
2023-10-18 13:40:31 -0700

    task 4 function

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - function ton check if c is a letter, lowercase or uppeercase
+ * 
+ * c: is the int that will use for the argument of function
+ *
+ * Return:0
+ */
+
+int _isalpha(int c)
+
+{
+	if ((c >= 'a' && c <= 'z') | (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	return (0);
+}
