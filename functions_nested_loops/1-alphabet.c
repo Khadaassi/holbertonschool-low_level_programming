fe60f05a94d76d7312eb1b738c5790086ecc13cf
Khadaassi <aassikhadija@yahoo.fr>
2023-10-18 11:10:48 -0700

    task 1 function

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,36 @@
+#include <unistd.h>
+/**
+ * main - print alphabeet in lowercase
+ *
+ * Description: print alphabet in lowercase using _putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+/**
+ * _putchar - prints char
+ */
+void _putchar(char c)
+{
+	write(1, &c, 1);
+}
+/**
+ * print_alphabet - print alphabet in lower case
+ */
+void print_alphabet(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	{
+	_putchar(letter);
+	letter++;
+	}
+
+	_putchar('\n');
+}
+
+	print_alphabet();
+	return (0);
+}
