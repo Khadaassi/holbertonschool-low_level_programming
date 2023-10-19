024c38370ac6ad308fb5fcb9c86a433d529eb54d
Khadaassi <aassikhadija@yahoo.fr>
2023-10-19 11:30:46 -0700

    task 7 function

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -3,7 +3,7 @@
 /**
  * print_last_digit - function that prints the last digit of a number
  *
- * c : is the int that will use for the argument of the function
+ * @c: is the int that will use for the argument of the function
  *
  * Return: 0
  */
@@ -14,7 +14,7 @@ int print_last_digit(int c)
 	if (c > 0 || c == 0)
 	{
 	_putchar (c % 10 + '0');
-	return ( c % 10);
+	return (c % 10);
 	}
 	else
 	{
