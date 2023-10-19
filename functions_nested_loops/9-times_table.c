072bea37e0974dd95b7e3796782a4b3207d41a50
Khadaassi <aassikhadija@yahoo.fr>
2023-10-19 13:53:43 -0700

    task 9 function

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * time_table - function to print the time table
+ *time_table - function to print the time table
  */
 
 void times_table(void)
@@ -10,14 +10,15 @@ void times_table(void)
 	int i;
 	int j;
 	for (i = 0; i <= 9; i++)
+
 	{
 		for (j = 0; j <= 8; j++)
 		{
-			printf("%2d,  ", i * j);
+			printf("%2d, ", i * j);
 		}
 		if (j == 9)
 		{
-		printf("%2d\n", i*j);
+		printf("%2d\n", i * j);
 		}
 	}
 	}
