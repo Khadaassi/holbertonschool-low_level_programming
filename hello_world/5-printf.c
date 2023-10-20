52850e4378d76f0e62e86e3cbf66eef0d5321233
Khadaassi <aassikhadija@yahoo.fr>
2023-10-20 12:51:42 -0700

    task 5 low level prog

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include<stdio.h>
 
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "with proper grammar,
-* but the outcome is a piece of art,"
-*/
-	int main(void)
+ * main - print the string in the put function
+ *
+ * Description: using the main function this program prints "with proper grammar,
+ *  but the outcome is a piece of art"
+ */
+
+ int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 		return (0);
