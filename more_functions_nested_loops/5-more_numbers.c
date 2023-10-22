442779cbded979894d3b617e21924131d812053e
Khadaassi <aassikhadija@yahoo.fr>
2023-10-22 08:44:54 -0700

    task 6 more function

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * more_numbers - fonction that prints 10 times the numbers from 0 to 14
+ */
+
+void more_numbers(void)
+
+{
+	char c = 0;
+
+	while (c <= '14')
+	{
+		putchar(c);
+		c++;
+	}
+		putchar('\n');
+}
