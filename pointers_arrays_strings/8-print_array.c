c336750c16f27840e4c0109b17c0919c808f1617
Khadaassi <aassikhadija@yahoo.fr>
2023-10-26 05:13:18 -0700

    task 8 pointer

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -3,6 +3,7 @@
  * print_array - function that print elements of an array.
  *
  * @n: number of elements of the array
+ * @a: array of integers
  */
 
 void print_array(int *a, int n)
@@ -10,6 +11,8 @@ void print_array(int *a, int n)
 {
 	int i;
 
+	if (n <= 0)
+		printf("\n");
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", a[i]);
