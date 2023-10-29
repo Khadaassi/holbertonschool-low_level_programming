3f15b3b9979bc5434cc28e7703c356195446f88a
Khadaassi <aassikhadija@yahoo.fr>
2023-10-29 00:53:32 -0700

    task 0 more pointers

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -5,11 +5,23 @@
  * _strcat - function that concatenates 2 strings
  * @dest: 1st string
  * @src: 2nd string
- * Return: dest
+ * Return: result
  */
 
 char *_strcat(char *dest, char *src)
 {
-	strcat(dest, src);
-	return (dest);
+	char *result = dest;
+
+	while (*dest != '\0')
+		dest++;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+
+	return (result);
 }
