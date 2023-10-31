fee1872cd276ad5aaa5062568037a10cb4d27647
Khadaassi <aassikhadija@yahoo.fr>
2023-10-31 08:19:01 -0700

    task 4 recursion

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,9 +1,10 @@
 #include "main.h"
 
 /**
- * _pow_recursion - function
- * @x:
- * @y:
+ * _pow_recursion - function that returns the value of a number raised
+ * @x: first number
+ * @y: puissance
+ *  Return: the value of x raised to the power of y
  */
 
 int _pow_recursion(int x, int y)
