4459032dc73e3904ed34f6fb04a1a00115535fa5
Khadaassi <aassikhadija@yahoo.fr>
2023-11-03 07:24:51 -0700

    task 1 arg

---
+++ argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - function that prints the number of arguments
+ * @argc: parameter for the number of arguments
+ * @argv: parameter for the list of arguments
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+
+	printf("%d\n",argc);
+	return (0);
+}
