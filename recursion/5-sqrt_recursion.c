66b3a3bb5be950d9637856949a8e5fa283f91159
Khadaassi <aassikhadija@yahoo.fr>
2023-11-03 15:41:51 -0700

    task 5 recursion

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,33 +1,36 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the square root of a number
- * @n: parameter of the function
- * Return: square root of the number
+ *_sqrt_recursion -  func that returns the natural square root of a number.
+ * @n: for number input
+ * Return: 0
  */
 
 int _sqrt_recursion(int n)
-
 {
-	int sqrtn = _sqrt_recursion(n - 1);
-
-	if (n < 0)
-	{
+	if (n == 0)
+		return (0);
+	else if (n == 1)
+		return (1);
+	else if (n < 0)
 		return (-1);
-	}
-	else if (n == 0 || n == 1)
-	{
-		return (n);
-	}
 	else
-	{
-		if (sqrtn * sqrtn <= n && (sqrtn + 1) * (sqrtn + 1) > n)
-		{
-			return (sqrtn);
-		}
-		else
-		{
-			return (-1);
-		}
-	}
+		return (helper(n, 1));
+}
+
+/**
+ * helper - blank
+ * @n: number
+ * @i: incrementor
+ * Return: -1
+ */
+
+int helper(int n, int i)
+{
+	if (n == (i * i))
+		return (i);
+	else if (n > (i * i))
+		return (helper(n, i + 1));
+	else
+		return (-1);
 }
