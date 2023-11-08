921f928b31ab1b1e01a2d3ac9a21784d0a9b65c6
khadouj <aassikhadija@gmail.com>
2023-11-08 20:57:35 +0100

    task 2 malloc

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,44 +5,46 @@
   * @s2: string 2
   * Return: result
   */
-char *str_concat(char *s1, char *s2) 
+char *str_concat(char *s1, char *s2)
 {
-    int len1 = 0;
-    int len2 = 0;
-    char *result;
-    int i, j;
+	int len1 = 0;
+	int len2 = 0;
+	char *result;
+	int i, j;
 
-    if (s1 == NULL) s1 = "";
-    if (s2 == NULL) s2 = "";
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
-     while (s1[len1] != '\0')
-    {
-        len1++;
-    }
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
 
-    while (s2[len2] != '\0')
-    {
-        len2++;
-    }
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
 
-    result = (char *)malloc(len1 + len2 + 1);
+	result = (char *)malloc(len1 + len2 + 1);
 
-    if (result == NULL)
-    {
-        return NULL;
-    }
+	if (result == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < len1; i++)
-    {
-        result[i] = s1[i];
-    }
+	for (i = 0; i < len1; i++)
+	{
+		result[i] = s1[i];
+	}
 
-    for (j = 0; j < len2; j++)
-    {
-      result[i + j] = s2[j];
-    }
+	for (j = 0; j < len2; j++)
+	{
+		result[i + j] = s2[j];
+	}
 
-  result[i + j] = '\0';
+	result[i + j] = '\0';
 
-    return result;
+	return (result);
 }
