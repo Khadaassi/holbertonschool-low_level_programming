c03eef69d17a129fe7b4472ae14129c3840a27a7
khadouj <aassikhadija@gmail.com>
2023-11-09 12:18:07 +0000

    task 4 malloc

---
+++ malloc_free/alloc_grid.h
@@ -0,0 +1,7 @@
+#ifndef ALLOC_GRID_H
+#define ALLOC_GRID_H
+
+int **alloc_grid(int width, int height);
+
+#endif
+
---
+++ malloc_free/free_grid.h
@@ -0,0 +1,7 @@
+#ifndef FREE_GRID_H
+#define FREE_GRID_H
+
+void free_grid(int **grid, int height);
+
+#endif
+
