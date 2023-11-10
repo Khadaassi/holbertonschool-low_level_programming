b2882242fdfbda1908ffcf19584437c0dbf064d6
khadouj <aassikhadija@gmail.com>
2023-11-10 20:06:56 +0100

    advanced

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * argstostr - function that concatenates arguments of the prog
+ * @ac:
+ * @av:
+ * Return: NULL or pointer to a new string
+*/
+char *argstostr(int ac, char **av)
+{
+    
+}
