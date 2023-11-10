451db78c766bbd82945a52e0c80978f8a6811308
khadouj <aassikhadija@gmail.com>
2023-11-10 20:26:24 +0100

    advanced

--- malloc_free/100-argstostr.c
+++ malloc_free/100-argstostr.c
@@ -1,22 +1,25 @@
 #include "main.h"
+
 /**
  * argstostr - function that concatenates arguments of the prog
  * @ac: parameter for the number of arguments
  * @av: parameter for the list of arguments
  * Return: NULL or pointer to a new string
 */
-char *argstostr(int ac, char **av)
-{
-    int i;
-    char *str;
 
-    if (ac == 0 || av == 0)
-        return (NULL);
+char *argstostr(int ac, char **av)
+
+{
+	int i;
+	char *str;
+
+	if (ac == 0 || av == 0)
+		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
 		printf("%s\n", av[i]);
 	}
-    str = (char*)malloc(sizeof(av[i]));
+	str = (char *)malloc(sizeof(av[i]));
 	return (str);
 }
