8dc12c77264fba788d03b7dbd0b0556b56816b80
khadouj <aassikhadija@gmail.com>
2023-11-14 11:11:53 +0100

    task 5 structure

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * free_dog - Frees memory for a dog structure
+ * @d: Pointer to the dog structure
+ */
+void free_dog(dog_t *d)
+{
+	if (d != NULL)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
