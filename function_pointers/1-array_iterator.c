c17c4197f4f6016a42ca869f9596189760a6074f
khadouj <aassikhadija@gmail.com>
2023-11-15 19:22:46 +0100

    task 1 function et pointer

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,12 +1,10 @@
 #include "function_pointers.h"
 /**
- *
- * @array
- * @size
- * @paction
-*/
-
-
+ * array_iterator - Applies a function to each element of an array.
+ * @array: The array to iterate over.
+ * @size: The size of the array.
+ * @action: The function to apply to each element.
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 
 {
