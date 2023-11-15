230a317ef4a7cd788e4edcaaaf100b5065c5daab
khadouj <aassikhadija@gmail.com>
2023-11-15 14:00:34 +0100

    task 0 function_pointers

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * print_name - Prints a name.
+ * @name: The name to be printed
+ * @f: Function pointer to the print function
+ *
+ * Calls the specified print function (@f) to print the name.
+ * After that, prints the name character by character and adds a newline.
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+
+	while (*name)
+	{
+		putchar(*name);
+		name++;
+	}
+	putchar('\n');
+}
