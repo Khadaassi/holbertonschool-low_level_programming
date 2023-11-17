3750c6cbd3f536d7cf2d79bed83dbd4b7fe5deac
khadouj <aassikhadija@gmail.com>
2023-11-17 15:41:29 +0100

    task 2 variadic

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -1,10 +1,12 @@
 #include "variadic_functions.h"
+
 /**
 * print_strings - Prints strings, followed by a new line.
 * @separator: The string to be printed between the strings (or NULL).
 * @n: The number of strings passed to the function.
 * @...: Variable number of strings to be printed.
 */
+
 void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list args;
