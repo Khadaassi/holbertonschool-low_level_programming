74456c48bc16ea17aa1c5d65fddaafcb98af8f87
Khadaassi <aassikhadija@gmail.com>
2023-12-01 10:32:18 +0100

    task 4 project 2 double linked lists

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * free_dlistint - Frees a dlistint_t linked list.
+ * @head: A pointer to the head of the doubly linked list.
+ *
+ * Description: Frees each node in the doubly linked list and sets the head
+ *              pointer to NULL.
+ */
+void free_dlistint(dlistint_t *head)
+{
+    dlistint_t *temp;
+
+    while (head != NULL)
+    {
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
