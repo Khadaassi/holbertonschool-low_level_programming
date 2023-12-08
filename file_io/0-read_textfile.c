2390d23bee38c97505d2dd8ceca80415233a918e
Khadaassi <aassikhadija@gmail.com>
2023-12-08 23:18:57 +0100

    task 1 init hash tables

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -4,7 +4,6 @@
  * standard output.
  * @filename: The name of the file to be read.
  * @letters: The number of letters to read and print.
- *
  * Return: The actual number of letters read and printed, 0 on failure.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
